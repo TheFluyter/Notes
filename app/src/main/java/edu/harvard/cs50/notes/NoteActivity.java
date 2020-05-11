0964256eed41b070511ee523f455a24a6a730ab4
TheFluyter <rick__ruiter@hotmail.com>
2020-05-11 19:55:09 +0200

    New delete button

--- app/src/main/java/edu/harvard/cs50/notes/NoteActivity.java
+++ app/src/main/java/edu/harvard/cs50/notes/NoteActivity.java
@@ -2,9 +2,14 @@ package edu.harvard.cs50.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NoteActivity extends AppCompatActivity {
     private EditText editText;
@@ -15,8 +20,18 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note);
 
         Intent intent = getIntent();
+        final int id = intent.getIntExtra("id", 0);
         editText = findViewById(R.id.note_edit_text);
         editText.setText(intent.getStringExtra("content"));
+
+        FloatingActionButton fab = findViewById(R.id.delete_note_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.database.noteDao().delete(id);
+                finish();
+            }
+        });
     }
 
     @Override
